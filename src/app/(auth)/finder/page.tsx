88e1bf2c0f25c162dc597330ccb52b5a0e08ebfe
root <root@campos48>
2025-05-18 12:04:37 -0300

    style: layout direction

--- src/app/(auth)/finder/page.tsx
+++ src/app/(auth)/finder/page.tsx
@@ -24,6 +24,7 @@ import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Aurora from "../../../../assets/bg-aurora";
+import { Search } from "lucide-react";
 
 const apiBrasil = process.env.NEXT_PUBLIC_BRASIL_API;
 const apiBrasilClima = process.env.NEXT_PUBLIC_BRASIL_API_CLIMA;
@@ -31,6 +32,7 @@ const apiBrasilClima = process.env.NEXT_PUBLIC_BRASIL_API_CLIMA;
 const FinderPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [giveAddress, setAddress] = useState<[] | null>([]);
+  const [loadedDatas, setLoadedDatas] = useState<boolean | null>();
 
   const form = useForm({
     defaultValues: {
@@ -43,6 +45,8 @@ const FinderPage = () => {
       const searchCep = await axios.get(
         `${apiBrasil}/${form.getValues("cepValue")}`,
       );
+      const statusReq = searchCep.status === 200;
+      setLoadedDatas(statusReq);
       console.log(searchCep.data.state);
       if (searchCep) {
       }
@@ -63,34 +67,26 @@ const FinderPage = () => {
   }, []);
 
   return (
-    <main className="flex h-[100vh] p-10 items-center gap-10 max-w-[70%]">
-      <Card className="h-auto min-w-[500px] w-full text-gray-200 bg-stone-800 border-none">
-        <span className="px-4">
-          {isLoading ? (
-            <Skeleton className="h-[16px] w-full" />
-          ) : (
-            <small>
-              Adicionar o background do React Bits: PARTICLES || AURORA
-            </small>
-          )}
-        </span>
+    <main className="flex flex-col lg:w-[600px] h-[100vh] p-10 items-center gap-2 max-w-[70%]">
+      <Card className="lg:flex-col h-auto min-w-[400px] w-full text-gray-200 bg-stone-800 border-none">
         <CardHeader>
           <h2 className="text-2xl"> Localizador 48</h2>
         </CardHeader>
         <CardContent>
           <Form {...form}>
+            <small>Digite o cep</small>
             <form
-              className="flex flex-col gap-4"
+              className="flex justify-between w-full items-center"
               onSubmit={form.handleSubmit(getCep)}
             >
               <FormField
                 control={form.control}
                 name="cepValue"
                 render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Digite o cep</FormLabel>
+                  <FormItem className="flex items-center">
                     <FormControl>
                       <Input
+                        className="h-[50px] w-100 border-none bg-stone-900"
                         type="number"
                         placeholder="Cep ex: 08033219..."
                         {...field}
@@ -101,11 +97,7 @@ const FinderPage = () => {
                         className={
                           form.watch("cepValue").length > 0 ? "p-2" : null
                         }
-                      >
-                        {form.watch("cepValue").length > 0 ? (
-                          <small>searching...</small>
-                        ) : null}
-                      </span>
+                      ></span>
                       {/* <Skeleton className="bg-stone-900 h-8 w-full" /> */}
                     </FormDescription>
                     <FormMessage title="Insira os dados corretamente" />
@@ -113,38 +105,65 @@ const FinderPage = () => {
                 )}
               />
               <Button
-                className="cursor-pointer"
+                className="cursor-pointer h-[40px] w-[50px]"
                 type="submit"
                 disabled={form.watch("cepValue").length < 8}
               >
-                Pesquisar
+                <Search size={30} />
               </Button>
             </form>
+            {form.watch("cepValue").length > 0 ? (
+              <small>searching...</small>
+            ) : null}
           </Form>
         </CardContent>
-        <CardFooter>rodapé</CardFooter>
+        <CardFooter>{loadedDatas ? null : new Date().getFullYear()}</CardFooter>
       </Card>
-      <Card className="h-auto min-w-[600px] w-full text-gray-200 bg-stone-800 border-none">
+      <Card className="h-auto min-w-[400px] max-w-[600px] w-full text-gray-200 bg-transparent border-none">
         <CardHeader>
-          Resultado da busca
-          <CardDescription>lorem</CardDescription>
+          {loadedDatas ? (
+            <>
+              Resultado da busca
+              <CardDescription>{`${new Date().getHours()}:${new Date().getMinutes()}h	`}</CardDescription>
+            </>
+          ) : (
+            <>{null}</>
+          )}
         </CardHeader>
         <CardContent>
-          {giveAddress && (
-            <section className="bg-stone-900 rounded-xl p-3 text-xl">
+          {loadedDatas ? (
+            <section className="bg-stone-900 min-h-[160px] rounded-xl text-xl">
               <p>
-                <b>{giveAddress.neighborhood}</b>
+                {isLoading ? (
+                  <Skeleton className="h-[16px] w-full" />
+                ) : (
+                  <b>{giveAddress.neighborhood}</b>
+                )}
               </p>
               <span key={giveAddress.cep}>
                 <p>
-                  {giveAddress.city}, {giveAddress.state}
+                  {isLoading ? (
+                    <Skeleton className="h-[16px] w-full" />
+                  ) : (
+                    <b>
+                      {giveAddress.city}, {giveAddress.state}
+                    </b>
+                  )}
                 </p>
-                <p className="text-lg">{giveAddress.street}</p>
+                <p className="text-lg">
+                  {isLoading ? (
+                    <Skeleton className="h-[16px] w-full" />
+                  ) : (
+                    <b>{giveAddress.street}</b>
+                  )}
+                </p>
               </span>
             </section>
+          ) : (
+            <section className="h-auto min-h-[160px]">{""}</section>
           )}
         </CardContent>
-        <CardFooter>rodapé 2</CardFooter>
+        <CardFooter>{""}</CardFooter>
       </Card>
     </main>
   );
