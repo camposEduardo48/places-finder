bc7f576cc199040ab5246e6d0f19abde4f5b2f1c
root <root@campos48>
2025-05-19 15:03:52 -0300

    style: fix

--- src/app/(auth)/finder/page.tsx
+++ src/app/(auth)/finder/page.tsx
@@ -15,7 +15,6 @@ import {
   FormDescription,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
@@ -23,15 +22,23 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Aurora from "../../../../assets/bg-aurora";
-import { Search } from "lucide-react";
+import { Building2, Clock, Loader, MapPin, Search } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 const apiBrasil = process.env.NEXT_PUBLIC_BRASIL_API;
 const apiBrasilClima = process.env.NEXT_PUBLIC_BRASIL_API_CLIMA;
 
+interface TypeApiBrasil {
+  cep: string;
+  state: string;
+  city: string;
+  neighborhood: string;
+  street: string;
+}
+
 const FinderPage = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [giveAddress, setAddress] = useState<[] | null>([]);
+  const [giveAddress, setAddress] = useState<TypeApiBrasil | undefined>();
   const [loadedDatas, setLoadedDatas] = useState<boolean | null>();
 
   const form = useForm({
@@ -69,9 +76,11 @@ const FinderPage = () => {
   return (
     <main className="flex flex-col lg:w-[600px] h-[100vh] p-10 items-center gap-2 max-w-[70%]">
       <Card className="lg:flex-col h-auto min-w-[400px] w-full text-gray-200 bg-stone-800 border-none">
-        <CardHeader>
+        <CardHeader className="flex items-center gap-3">
+          <MapPin size={30} color="gray" />
           <h2 className="text-2xl"> Localizador 48</h2>
         </CardHeader>
+        <Separator color="black" />
         <CardContent>
           <Form {...form}>
             <small>Digite o cep</small>
@@ -83,21 +92,21 @@ const FinderPage = () => {
                 control={form.control}
                 name="cepValue"
                 render={({ field }) => (
-                  <FormItem className="flex items-center">
+                  <FormItem className="flex mt-2 items-center">
                     <FormControl>
                       <Input
-                        className="h-[50px] w-100 border-none bg-stone-900"
+                        className="h-[50px] w-100 mt-0 border-none bg-stone-900"
                         type="number"
                         placeholder="Cep ex: 08033219..."
                         {...field}
                       />
                     </FormControl>
                     <FormDescription>
-                      <span
+                      <div
                         className={
                           form.watch("cepValue").length > 0 ? "p-2" : null
                         }
-                      ></span>
+                      />
                       {/* <Skeleton className="bg-stone-900 h-8 w-full" /> */}
                     </FormDescription>
                     <FormMessage title="Insira os dados corretamente" />
@@ -105,7 +114,7 @@ const FinderPage = () => {
                 )}
               />
               <Button
-                className="cursor-pointer h-[40px] w-[50px]"
+                className="cursor-pointer h-full w-auto"
                 type="submit"
                 disabled={form.watch("cepValue").length < 8}
               >
@@ -113,18 +122,25 @@ const FinderPage = () => {
               </Button>
             </form>
             {form.watch("cepValue").length > 0 ? (
-              <small>searching...</small>
+              <small className="p-2">searching...</small>
             ) : null}
           </Form>
         </CardContent>
-        <CardFooter>{loadedDatas ? null : new Date().getFullYear()}</CardFooter>
+        <CardFooter className="flex justify-end text-gray-500">
+          {loadedDatas ? null : <small>{new Date().getFullYear()}</small>}
+        </CardFooter>
       </Card>
-      <Card className="h-auto min-w-[400px] max-w-[600px] w-full text-gray-200 bg-transparent border-none">
+      <Card className="h-auto min-w-[400px] max-w-[600px] w-full text-gray-200 bg-transparent shadow-none border-none">
         <CardHeader>
           {loadedDatas ? (
             <>
               Resultado da busca
-              <CardDescription>{`${new Date().getHours()}:${new Date().getMinutes()}h	`}</CardDescription>
+              <CardDescription className="flex items-center gap-1">
+                <p>{`${new Date().getHours()}:${new Date().getMinutes()}h	`}</p>
+                <div>
+                  <Clock size="15" />
+                </div>
+              </CardDescription>
             </>
           ) : (
             <>{null}</>
@@ -132,30 +148,37 @@ const FinderPage = () => {
         </CardHeader>
         <CardContent>
           {loadedDatas ? (
-            <section className="bg-stone-900 min-h-[160px] rounded-xl text-xl">
+            <section className="flex flex-col gap-4 bg-stone-900 min-h-[160px] rounded-xl text-xl">
               <p>
                 {isLoading ? (
                   <Skeleton className="h-[16px] w-full" />
                 ) : (
-                  <b>{giveAddress.neighborhood}</b>
+                  <b>{giveAddress?.neighborhood}</b>
                 )}
               </p>
-              <span key={giveAddress.cep}>
+              <p className="text-4xl">
+                {isLoading ? (
+                  <Skeleton className="h-[16px] w-full" />
+                ) : (
+                  <b>{giveAddress?.street}</b>
+                )}
+              </p>
+              <span className="flex gap-3" key={giveAddress?.cep}>
                 <p>
                   {isLoading ? (
                     <Skeleton className="h-[16px] w-full" />
                   ) : (
                     <b>
-                      {giveAddress.city}, {giveAddress.state}
+                      {giveAddress?.city}, {giveAddress?.state}
                     </b>
                   )}
                 </p>
-                <p className="text-lg">
-                  {isLoading ? (
-                    <Skeleton className="h-[16px] w-full" />
-                  ) : (
-                    <b>{giveAddress.street}</b>
-                  )}
+              </span>
+              <span>
+                <p>
+                  <small className="text-stone-500">
+                    Cep: {giveAddress.cep}
+                  </small>
                 </p>
               </span>
             </section>
