ddf8644845c39e8f4f89483dc4de50df43c710de
root <root@campos48>
2025-05-28 16:00:28 -0300

    style: fix layout

--- src/app/(auth)/finder/page.tsx
+++ src/app/(auth)/finder/page.tsx
@@ -24,10 +24,13 @@ import { motion } from "framer-motion";
 import axios from "axios";
 import dayjs from "dayjs";
 import { Clock, Loader2, MapPin, Search } from "lucide-react";
+import { Signpost, LocationCity, MarkunreadMailbox } from "@mui/icons-material";
 import { Separator } from "@/components/ui/separator";
+import { useTheme } from "next-themes";
 
 const apiBrasil = process.env.NEXT_PUBLIC_BRASIL_API;
 // const apiBrasilClima = process.env.NEXT_PUBLIC_BRASIL_API_CLIMA;
+const actualDate = dayjs(new Date()).format("HH:mm - DD/MM/YYYY");
 
 interface TypeApiBrasil {
   cep: string;
@@ -38,7 +41,8 @@ interface TypeApiBrasil {
 }
 
 const FinderPage = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  const { setTheme } = useTheme();
+  const [isLoading, setIsLoading] = useState(false);
   const [giveAddress, setAddress] = useState<TypeApiBrasil>();
   const [loadedDatas, setLoadedDatas] = useState<boolean | null>();
 
@@ -49,6 +53,7 @@ const FinderPage = () => {
   });
 
   const getCep = async () => {
+    setIsLoading(false);
     try {
       const searchCep = await axios.get(
         `${apiBrasil}/${form.getValues("cepValue")}`,
@@ -67,17 +72,19 @@ const FinderPage = () => {
   };
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 1500);
-
+    // const timer = setTimeout(() => {
+    //   setIsLoading(false);
+    // }, 1500);
     //cleanup function
-    return () => clearTimeout(timer);
+    // return () => clearTimeout(timer);
   }, []);
 
   return (
-    <main className="flex flex-col lg:w-[600px] h-[100vh] p-10 items-center gap-2 max-w-[70%]">
-      <Card className="lg:flex-col h-auto min-w-[400px] w-full text-gray-200 bg-stone-800 border-none">
+    <main
+      onLoad={() => setTheme("dark")}
+      className="box-content flex flex-col gap-6 lg:w-[600px] h-[100vh] max-sm:justify-center justify-center max-sm:items-center items-center max-sm:w-full lg:max-w-[70%]"
+    >
+      <Card className="h-auto min-w-[350px] max-w-[600px] w-[96%] text-gray-200 bg-stone-800 shadow-none">
         <CardHeader className="flex items-center gap-3">
           <MapPin size={30} color="gray" />
           <h2 className="text-2xl"> Localizador 48</h2>
@@ -87,17 +94,17 @@ const FinderPage = () => {
           <Form {...form}>
             <small>Digite o cep</small>
             <form
-              className="flex justify-between w-full items-center"
+              className="flex max-sm:flex-col border-2 border-pink-500 justify-between w-full"
               onSubmit={form.handleSubmit(getCep)}
             >
               <FormField
                 control={form.control}
                 name="cepValue"
                 render={({ field }) => (
-                  <FormItem className="flex mt-2 items-center">
+                  <FormItem className="flex border-2 border-b-blue-400 mt-2 items-center">
                     <FormControl>
                       <Input
-                        className="h-[50px] w-100 mt-0 border-none bg-stone-900"
+                        className="h-[50px] w-full mt-0 border-none bg-stone-900"
                         type="number"
                         placeholder="Cep ex: 08033219..."
                         {...field}
@@ -113,7 +120,7 @@ const FinderPage = () => {
                 )}
               />
               <Button
-                className={`cursor-pointer h-full w-auto ${form.watch("cepValue").length === 8 ? "border-2 border-[#00ff00]" : "none"}`}
+                className={`cursor-pointer dark:bg-stone-900 p-3 h-full w-auto ${form.watch("cepValue").length === 8 ? "border-2 border-[#00ff00]" : "none"}`}
                 type="submit"
                 disabled={form.watch("cepValue").length < 8}
               >
@@ -125,6 +132,7 @@ const FinderPage = () => {
                     color={
                       form.watch("cepValue").length === 8 ? "#00ff00" : "#fff"
                     }
+                    className="h-5 w-5"
                     size={30}
                   />
                 )}
@@ -141,83 +149,80 @@ const FinderPage = () => {
           <small>Sopmac&copy; {new Date().getFullYear()}</small>
         </CardFooter>
       </Card>
-      <Card className="h-auto min-w-[400px] max-w-[600px] w-full text-gray-200 bg-transparent shadow-none border-none">
-        <CardHeader>
-          {loadedDatas ? (
-            <section>
-              Resultado da busca
-              <CardDescription className="flex items-center gap-1">
-                <Suspense fallback={<p>Loading tararan...</p>}>
-                  <p>{`${dayjs(new Date()).format("HH:mm - DD/MM/YYYY")}`}</p>
-                  <span>
-                    <Clock size="15" />
-                  </span>
-                </Suspense>
-              </CardDescription>
-            </section>
-          ) : (
-            <>{null}</>
-          )}
-        </CardHeader>
-        <CardContent className="w-full">
-          {loadedDatas || form.watch("cepValue").length > 0 ? (
-            <section className="flex flex-col gap-4 bg-stone-900 min-h-[300px] h-auto max-h-[500px] overflow-y-auto w-full rounded-xl text-xl">
-              <motion.div animate={{ y: 20 }}>
-                <p>
-                  {isLoading ? (
-                    <Skeleton className="h-[20px] w-full" />
-                  ) : (
-                    <b>{giveAddress?.neighborhood}</b>
-                  )}
+      {form.watch("cepValue").padStart(1) && (
+        <Card className="h-auto min-w-[350px] max-w-[600px] w-[96%] text-gray-200 bg-stone-800 shadow-none">
+          <CardHeader className="w-full text-white">
+            {loadedDatas && (
+              <>
+                <p className="text-lg">Resultado da pesquisa</p>
+                <p className="text-stone-500">
+                  <small>{actualDate}</small>
                 </p>
-                <p className="text-4xl">
-                  {isLoading ? (
-                    <Skeleton className="h-[40px] bg-stone-600 w-full" />
-                  ) : (
-                    <b>{giveAddress?.street}</b>
+              </>
+            )}
+          </CardHeader>
+          {isLoading ? <Separator /> : null}
+          <CardContent className="w-full p-0">
+            {loadedDatas || form.watch("cepValue").length > 0 ? (
+              <section className="flex flex-col items-start justify-start p-4 gap-4 bg-stone-800 min-h-[300px] h-auto max-h-[500px] overflow-y-auto w-full rounded-xl text-xl">
+                <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }}>
+                  {!loadedDatas && (
+                    <Loader2
+                      className="w-[100px] animate-spin text-muted-foreground"
+                      size={100}
+                    />
                   )}
-                </p>
-                <div>
-                  {isLoading ? (
-                    <Skeleton className="h-[40px] bg-stone-600 w-full" />
-                  ) : (
-                    <p>{`${giveAddress?.city}, ${giveAddress?.state}`}</p>
-                  )}
-                </div>
-                <span>
-                  {isLoading ? (
-                    <Skeleton className="h-[16px] w-full" />
-                  ) : (
-                    <p>
-                      <small className="text-stone-500">
-                        Cep: {giveAddress?.cep}
-                      </small>
-                    </p>
-                  )}
-                </span>
-                {/* meu loader de carregamento */}
-                {/* <span
-                  className="flex items-center justify-center"
-                  key={giveAddress?.cep}
-                >
-                  <div>
-                    {isLoading ? (
-                      <Skeleton className="h-[16px] w-full" />
-                    ) : (
-                      <div className="flex items-center justify-center w-100 border">
-                        <Loader2 className="h-20 w-20 animate-spin text-stone-600" />
-                      </div>
-                    )}
-                  </div>
-                </span> */}
-              </motion.div>
-            </section>
-          ) : (
-            <section className="h-auto min-h-[160px]">{""}</section>
-          )}
-        </CardContent>
-        <CardFooter>{""}</CardFooter>
-      </Card>
+                  <ul>
+                    <li className="pb-4">
+                      {isLoading ? (
+                        <Skeleton className="h-[20px] w-full" />
+                      ) : (
+                        <span className="flex items-center gap-2">
+                          <LocationCity />
+                          <p>
+                            <small>Bairro</small>
+                          </p>
+                        </span>
+                      )}
+                      {giveAddress?.neighborhood}
+                    </li>
+                    <li className="pb-5">
+                      <span className="flex items-center gap-2">
+                        <Signpost />
+                        <p>
+                          <small>Rua/Avenida</small>
+                        </p>
+                      </span>
+                      <p className="text-3xl">
+                        {isLoading ? (
+                          <Skeleton className="h-[40px] bg-stone-600 w-full" />
+                        ) : (
+                          <b>{giveAddress?.street}</b>
+                        )}
+                      </p>
+                    </li>
+                    <li className="pb-3">
+                      <span className="flex items-center gap-2">
+                        <MarkunreadMailbox />
+                        <p>
+                          <small>Cep</small>
+                        </p>
+                      </span>
+                      <p>
+                        {loadedDatas && (
+                          <small className="text-stone-500">
+                            {giveAddress?.cep}
+                          </small>
+                        )}
+                      </p>
+                    </li>
+                  </ul>
+                </motion.div>
+              </section>
+            ) : null}
+          </CardContent>
+        </Card>
+      )}
     </main>
   );
 };
