2d57165f9f3bb5516328971a38f4caec3e0698a2
root <root@campos48>
2025-06-09 14:05:48 -0300

    feat: icons

--- src/app/(auth)/finder/page.tsx
+++ src/app/(auth)/finder/page.tsx
@@ -1,10 +1,8 @@
 "use client"
 
-// import "dotenv/config"
+import "dotenv/config"
 import { Button } from "@/components/ui/button"
-// console.log(Button)
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
-//console.log(Card, CardContent, CardFooter, CardHeader)
 import {
   Form,
   FormControl,
@@ -12,16 +10,13 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form"
-// console.log(Form, FormControl, FormField, FormItem, FormMessage)
 import { Input } from "@/components/ui/input"
 import { Skeleton } from "@/components/ui/skeleton"
 import { useForm } from "react-hook-form"
 import { useState } from "react"
-// import { motion } from "framer-motion"
-// console.log(Input, Skeleton, useForm, useEffect, useState, motion)
+import { motion } from "framer-motion"
 import axios from "axios"
-// import dayjs from "dayjs"
-// console.log(axios, dayjs)
+import dayjs from "dayjs"
 import {
   BookMarked,
   Clock,
@@ -29,14 +24,15 @@ import {
   Signpost,
   MapPin as Locate,
   Search,
+  MapPin,
 } from "lucide-react"
 // console.log(BookMarked, Clock, Loader2, Locate, Signpost, MapPin, Search)
 import { Separator } from "@/components/ui/separator"
 // console.log(Separator)
 // import { useTheme } from "next-themes";
-// const apiBrasil = process.env.NEXT_PUBLIC_BRASIL_API
-// const apiBrasilClima = process.env.NEXT_PUBLIC_BRASIL_API_CLIMA;
-// const actualDate = dayjs(new Date()).format("HH:mm - DD/MM/YYYY")
+const apiBrasil = process.env.NEXT_PUBLIC_BRASIL_API
+const actualDate = dayjs(new Date()).format("HH:mm - DD/MM/YYYY")
+// const img = "../../../../assets/bg-locations.svg"
 
 interface TypeApiBrasil {
   cep: string
@@ -58,11 +54,15 @@ const FinderPage = () => {
   })
 
   const getCep = async () => {
+    if (giveAddress) {
+      // setAddress(null)
+      setLoadedDatas(false)
+    }
     setIsLoading(true)
     try {
       setTimeout(async () => {
         const searchCep = await axios.get(
-          `${"https://brasilapi.com.br/api/cep/v1"}/${form.getValues("cepValue")}`,
+          `${apiBrasil}/${form.getValues("cepValue")}`,
         )
         const statusReq = searchCep.status === 200
         setLoadedDatas(statusReq)
@@ -86,14 +86,11 @@ const FinderPage = () => {
     <main className="box-content flex flex-col gap-6 lg:w-[600px] h-[100vh] max-sm:justify-center justify-center max-sm:items-center items-center max-sm:w-full lg:max-w-[70%]">
       <Card className="h-auto min-w-[350px] max-w-[600px] w-[96%] text-gray-200 bg-stone-800 shadow-none">
         <CardHeader className="flex items-center gap-3">
-          {/* <MapPin size={30} color="gray" /> */}
-          <h2 className="text-2xl"> Localizador 48</h2>
-          <p>
-            <small>
-              erro build pode estar relacionado a importação de arquivos
-              incorreta ou com a condição ternaria
-            </small>
-          </p>
+          <MapPin size={34} className="text-stone-500" />
+          <h2 className="text-2xl">
+            {" "}
+            Localizador <b className="text-blue-500">SOPMAC</b>
+          </h2>
           {/* Results: {count}
           <Button className="cursor-pointer" type="button" onClick={increment}>
             Increment
@@ -107,19 +104,16 @@ const FinderPage = () => {
               className="flex gap-3 justify-start h-auto w-full items-center"
               onSubmit={form.handleSubmit(getCep)}
             >
-              <small>Digite o cep</small>
               <FormField
                 control={form.control}
                 name="cepValue"
                 render={({ field }) => (
-                  <FormItem
-                    className={`flex w-full rounded-lg ${form.watch("cepValue").length === 8 ? "border-2 border-b-[#00ff00]" : "none"} mt-2 items-center`}
-                  >
+                  <FormItem className="flex w-full rounded-lg mt-2 items-center">
                     <FormControl>
                       <Input
                         className="h-[50px] w-[100%] mt-0 border-none bg-stone-900"
                         type="number"
-                        placeholder="Cep ex: 08033219..."
+                        placeholder="Digite um cep. Ex: 08033219..."
                         {...field}
                       />
                     </FormControl>
@@ -129,7 +123,7 @@ const FinderPage = () => {
               />
               {/* <Button type="reset">remove</Button> */}
               <Button
-                className={`cursor-pointer dark:bg-stone-900 p-3 h-full border-2 w-auto ${form.watch("cepValue").length === 8 ? "border-2 border-[#00ff00]" : "none"}`}
+                className={`cursor-pointer dark:bg-stone-900 p-3 h-full border-2 w-auto ${form.watch("cepValue").length === 8 ? "border-2 border-blue-500" : "none"}`}
                 type="submit"
                 disabled={form.watch("cepValue").length < 8}
               >
@@ -139,7 +133,7 @@ const FinderPage = () => {
                 ) : (
                   <Search
                     color={
-                      form.watch("cepValue").length === 8 ? "#00ff00" : "#fff"
+                      form.watch("cepValue").length === 8 ? "#2B7FFF" : "#fff"
                     }
                     className="h-5 w-5"
                     size={30}
@@ -163,10 +157,10 @@ const FinderPage = () => {
           <CardHeader className="w-full text-white">
             {loadedDatas && (
               <>
-                <p className="text-lg">Resultado da pesquisa</p>
+                <p className="text-lg text-stone-400">Resultado da pesquisa</p>
                 <p className="flex gap-2 items-center text-stone-500">
                   <Clock size={16} />
-                  <small>{"actualDate"}</small>
+                  <small>{actualDate}</small>
                 </p>
               </>
             )}
@@ -175,66 +169,73 @@ const FinderPage = () => {
           <CardContent className="w-full p-0">
             {loadedDatas || form.watch("cepValue").length > 0 ? (
               <section className="flex flex-col justify-center items-center p-4 gap-4 bg-stone-800 min-h-[300px] h-auto max-h-[500px] overflow-y-auto w-full rounded-xl text-xl">
-                {/* <motion.div initial={{ scale: -0.5 }} animate={{ scale: 1 }}> */}
-                {form.watch("cepValue").length <= 7 ||
-                  (!loadedDatas && (
-                    <Loader2
-                      className="relative w-[100px] animate-spin text-muted-foreground"
-                      size={100}
-                    />
-                  ))}
-                {loadedDatas && (
-                  <ul className=" flex flex-col items-start justify-start w-full">
-                    <li className="pb-4 w-[100%]">
-                      {!loadedDatas ? (
-                        <Skeleton className="h-[20px] w-full" />
-                      ) : (
-                        <span className="flex items-center gap-2">
-                          <Locate />
-                          <p>
-                            <small>Bairro</small>
-                          </p>
-                        </span>
-                      )}
-                      {giveAddress?.neighborhood}
-                    </li>
-                    <li className="pb-5">
-                      <span className="flex items-center gap-2">
-                        <Signpost />
-                        <p>
-                          <small>Rua/Avenida</small>
-                        </p>
-                      </span>
-                      <div className="text-3xl">
-                        {!loadedDatas ? (
-                          <Skeleton className="h-[40px] bg-stone-600 w-full" />
-                        ) : (
-                          <p>{giveAddress?.street}</p>
-                        )}
+                <motion.div initial={{ scale: -0.5 }} animate={{ scale: 1 }}>
+                  {form.watch("cepValue").length <= 7 ||
+                    (!loadedDatas && (
+                      <Loader2
+                        className="relative w-[100px] animate-spin text-muted-foreground"
+                        size={100}
+                      />
+                    ))}
+                  {loadedDatas && (
+                    <section className="flex gap-4">
+                      <ul className="flex flex-col items-start justify-start w-full">
+                        <li className="pb-4 w-full">
+                          {!loadedDatas ? (
+                            <Skeleton className="h-[20px] w-full" />
+                          ) : (
+                            <span className="flex items-center gap-1">
+                              <p className="text-stone-400 font-semibold">
+                                <small>Bairro</small>
+                              </p>
+                              <Locate className="text-stone-500" />
+                            </span>
+                          )}
+                          {giveAddress?.neighborhood}
+                        </li>
+                        <li className="pb-5">
+                          <span className="flex items-center gap-1">
+                            <p className="text-stone-400 font-semibold">
+                              <small>Rua/Avenida</small>
+                            </p>
+                            <Signpost className="text-stone-500" />
+                          </span>
+                          <div className="text-3xl">
+                            {!loadedDatas ? (
+                              <Skeleton className="h-[40px] bg-stone-600 w-full" />
+                            ) : (
+                              <p>{giveAddress?.street}</p>
+                            )}
+                          </div>
+                        </li>
+                        <li className="pb-3">
+                          <span className="flex items-center gap-1">
+                            <p className="text-stone-400 font-semibold">
+                              <small>Cep</small>
+                            </p>
+                            <BookMarked className="text-stone-500" />
+                          </span>
+                          <div>
+                            {!loadedDatas ? (
+                              <Skeleton className="h-[40px] bg-stone-600 w-full" />
+                            ) : (
+                              <p>
+                                <small>{giveAddress?.cep}</small>
+                              </p>
+                            )}
+                          </div>
+                        </li>
+                      </ul>
+                      <div className="flex items-center">
+                        <img
+                          src="https://img.icons8.com/fluency/100/location--v1.png"
+                          alt=""
+                          width={100}
+                        />
                       </div>
-                    </li>
-                    <li className="pb-3">
-                      <span className="flex items-center gap-2">
-                        <BookMarked />
-                        <p>
-                          <small>Cep</small>
-                        </p>
-                      </span>
-                      <div>
-                        {!loadedDatas ? (
-                          <Skeleton className="h-[40px] bg-stone-600 w-full" />
-                        ) : (
-                          <p>
-                            <small className="text-stone-500">
-                              {giveAddress?.cep}
-                            </small>
-                          </p>
-                        )}
-                      </div>
-                    </li>
-                  </ul>
-                )}
-                {/* </motion.div> */}
+                    </section>
+                  )}
+                </motion.div>
               </section>
             ) : null}
           </CardContent>
